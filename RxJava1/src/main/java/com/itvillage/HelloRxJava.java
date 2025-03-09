487a2ae3c552bd69e0f3d6362a8abe70abd8a487
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-03-09 22:55:15 +0900

    feat : 프로젝트 환경 구축 및 Hello RxJava

---
+++ RxJava1/src/main/java/com/itvillage/HelloRxJava.java
@@ -0,0 +1,11 @@
+package com.itvillage;
+
+import io.reactivex.Observable;
+
+public class HelloRxJava {
+
+    public static void main(String[] args) {
+        Observable<String> observable = Observable.just("Hello", "RxJava");
+        observable.subscribe(data -> System.out.println(data));
+    }
+}
