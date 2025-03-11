534d75a2060e743cc4633299930df71895b357d8
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-03-11 21:10:43 +0900

    feat : Observable과 Flowable에 대한 이해

---
+++ RxJava1/src/main/java/com/itvillage/chapter03/chapter0302/BackpressureBufferExample01.java
@@ -0,0 +1,33 @@
+package com.itvillage.chapter03.chapter0302;
+
+import com.itvillage.utils.LogType;
+import com.itvillage.utils.Logger;
+import com.itvillage.utils.TimeUtil;
+import io.reactivex.BackpressureOverflowStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.schedulers.Schedulers;
+
+import java.util.concurrent.TimeUnit;
+
+public class BackpressureBufferExample01 {
+    public static void main(String[] args){
+        System.out.println("# start : " + TimeUtil.getCurrentTimeFormatted());
+        Flowable.interval(300L, TimeUnit.MILLISECONDS)
+                .doOnNext(data -> Logger.log("#inverval doOnNext()", data))
+                .onBackpressureBuffer(
+                        2,
+                        () -> Logger.log("overflow!"),
+                        BackpressureOverflowStrategy.DROP_LATEST)
+                .doOnNext(data -> Logger.log("#onBackpressureBuffer doOnNext()", data))
+                .observeOn(Schedulers.computation(), false, 1)
+                .subscribe(
+                        data -> {
+                            TimeUtil.sleep(1000L);
+                            Logger.log(LogType.ON_NEXT, data);
+                        },
+                        error -> Logger.log(LogType.ON_ERROR, error)
+                );
+
+        TimeUtil.sleep(2800L);
+    }
+}
