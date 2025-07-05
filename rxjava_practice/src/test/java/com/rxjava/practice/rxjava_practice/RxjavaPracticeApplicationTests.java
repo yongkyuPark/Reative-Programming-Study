a898afc7e244a50ad3d31ef90a6f0d97d48f9a18
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-07-05 14:23:11 +0900

    feat : initial commit

---
+++ rxjava_practice/src/test/java/com/rxjava/practice/rxjava_practice/RxjavaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rxjava.practice.rxjava_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RxjavaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
