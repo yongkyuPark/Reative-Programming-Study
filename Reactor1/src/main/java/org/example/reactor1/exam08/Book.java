c8f253a69923bb3a1a3cb8657320407d0499daf8
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-07-06 20:58:01 +0900

    feat : Context의 특징

---
+++ Reactor1/src/main/java/org/example/reactor1/exam08/Book.java
@@ -0,0 +1,12 @@
+package org.example.reactor1.exam08;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Book {
+    private String isbn;
+    private String bookName;
+    private String author;
+}
---
+++ Reactor1/src/main/java/org/example/reactor1/exam08/ContextFetureExample04.java
@@ -0,0 +1,34 @@
+package org.example.reactor1.exam08;
+
+import org.example.reactor1.utils.Logger;
+import org.example.reactor1.utils.TimeUtils;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+/**
+ * Context의 특징
+ *  - inner Sequence 내부에서는 외부 Context에 저장된 데이터를 읽을 수 있다.
+ *  - inner Sequence 내부에서 Context에 저장된 데이터는 inner Sequence 외부에서 읽을 수 없다.
+ */
+public class ContextFetureExample04 {
+
+    public static void main(String[] args) {
+        String key1 = "id";
+        Mono.just("Kevin")
+                //.transformDeferredContextual((stringMono, contextView) -> contextView.get("job"))
+                .flatMap(name -> Mono.deferContextual(ctx ->
+                                Mono.just(ctx.get(key1) + ", " + name)
+                                        .transformDeferredContextual((mono, innerCtx) ->
+                                                mono.map(data -> data + ", " + innerCtx.get("job"))
+                                        )
+                                        .contextWrite(context -> context.put("job", "Software Engineer"))
+                        )
+                )
+                .publishOn(Schedulers.parallel())
+                .contextWrite(context -> context.put(key1, "itVillage"))
+                .subscribe(Logger::onNext);
+
+        TimeUtils.sleep(100L);
+    }
+
+}
