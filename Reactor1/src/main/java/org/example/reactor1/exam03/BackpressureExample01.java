ebea7cd70434830530e38e0534edebd13448055b
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-07-06 17:09:35 +0900

    feat : BackPressure Example

---
+++ Reactor1/src/main/java/org/example/reactor1/exam03/BackpressureExample01.java
@@ -0,0 +1,33 @@
+package org.example.reactor1.exam03;
+
+import org.example.reactor1.utils.Logger;
+import org.example.reactor1.utils.TimeUtils;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+
+/**
+ * Subscriber가 처리 가능한 만큼의 request 개수를 조절하는 Backpressure 예제
+ */
+public class BackpressureExample01 {
+
+    public static void main(String[] args) {
+        Flux.range(1, 5)
+                .doOnNext(Logger::doOnNext)
+                .doOnRequest(Logger::doOnRequest)
+                .subscribe(new BaseSubscriber<Integer>() {
+                    @Override
+                    protected void hookOnSubscribe(Subscription subscription) {
+                        request(1);
+                    }
+
+                    @Override
+                    protected void hookOnNext(Integer value) {
+                        TimeUtils.sleep(2000L);
+                        Logger.onNext(value);
+                        request(1);
+                    }
+                });
+    }
+
+}
