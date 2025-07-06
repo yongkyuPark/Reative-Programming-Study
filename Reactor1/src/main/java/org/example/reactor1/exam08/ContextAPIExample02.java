e9a6ce9995c87b3461b47df04724b768eb2272a0
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-07-06 20:41:19 +0900

    feat : 자주 사용하는 Context API

---
+++ Reactor1/src/main/java/org/example/reactor1/exam08/ContextAPIExample02.java
@@ -0,0 +1,32 @@
+package org.example.reactor1.exam08;
+
+import org.example.reactor1.utils.Logger;
+import org.example.reactor1.utils.TimeUtils;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.util.context.Context;
+
+/**
+ * Context API 예제 코드
+ *  - pullAll(ContextView) API 사용
+ */
+public class ContextAPIExample02 {
+
+    public static void main(String[] args) {
+        String key1 = "id";
+        String key2 = "name";
+        String key3 = "country";
+
+        Mono.deferContextual(ctx ->
+                        Mono.just("ID: " + " " + ctx.get(key1) + ", " + "Name: " + ctx.get(key2) +
+                                ", " + "Country: " + ctx.get(key3))
+                )
+                .publishOn(Schedulers.parallel())
+                .contextWrite(context -> context.putAll(Context.of(key2, "Kevin", key3, "Korea").readOnly()))
+                .contextWrite(context -> context.put(key1, "itVillage"))
+                .subscribe(Logger::onNext);
+
+        TimeUtils.sleep(100L);
+    }
+
+}
---
+++ Reactor1/src/main/java/org/example/reactor1/exam08/ContextAPIExample03.java
@@ -0,0 +1,31 @@
+package org.example.reactor1.exam08;
+
+import org.example.reactor1.utils.Logger;
+import org.example.reactor1.utils.TimeUtils;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.util.context.Context;
+
+/**
+ * ContextView API 예제 코드
+ *
+ */
+public class ContextAPIExample03 {
+
+    public static void main(String[] args) {
+        String key1 = "id";
+        String key2 = "name";
+
+        Mono.deferContextual(ctx ->
+                        Mono.just("ID: " + " " + ctx.get(key1) + ", "
+                                + "Name: " + ctx.get(key2) + ", "
+                                + "Job: " + ctx.getOrDefault("job", "Software Engineer"))
+                )
+                .publishOn(Schedulers.parallel())
+                .contextWrite(Context.of(key1, "itVillage", key2, "Kevin"))
+                .subscribe(Logger::onNext);
+
+        TimeUtils.sleep(100L);
+    }
+
+}
