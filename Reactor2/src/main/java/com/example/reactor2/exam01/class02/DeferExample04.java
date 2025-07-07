e8b64f74068129854c6315fc424c821a6283a21a
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-07-07 19:18:28 +0900

    feat : defer로 데이터 emit 미루기

---
+++ Reactor2/src/main/java/com/example/reactor2/exam01/class02/DeferExample04.java
@@ -0,0 +1,29 @@
+package com.example.reactor2.exam01.class02;
+
+import com.example.reactor2.utils.Logger;
+import com.example.reactor2.utils.TimeUtils;
+import reactor.core.publisher.Mono;
+
+/**
+ * Defer 사용 예제
+ *  - 원본 데이터 소스에서 emit 되는 데이터가 없을 경우에만 Mono.defer(this::sayDefault)가 실행된다.
+ */
+public class DeferExample04 {
+
+    public static void main(String[] args) {
+        Logger.info("# Start");
+        Mono<Object> mono =
+                Mono
+                        .empty()
+                        .switchIfEmpty(Mono.defer(DeferExample04::sayDefault));
+
+        TimeUtils.sleep(3000);
+        mono.subscribe(Logger::onNext);
+    }
+
+    private static Mono<String> sayDefault() {
+        Logger.info("# Say Hi");
+        return Mono.just("Hi");
+    }
+
+}
