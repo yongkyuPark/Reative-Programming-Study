9e2af93611a110bf4d80be58272abc1a0115ce2a
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-07-07 18:26:59 +0900

    feat : first commit

---
+++ Reactor2/src/main/java/com/example/reactor2/Reactor2Application.java
@@ -0,0 +1,13 @@
+package com.example.reactor2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reactor2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reactor2Application.class, args);
+	}
+
+}
