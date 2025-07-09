130a83c1da4795d8fd892da228baa90ef12c55ad
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-07-09 21:18:25 +0900

    feat : 데이터 결합 - concat

---
+++ Reactor2/src/main/java/com/example/reactor2/exam03/class04/ConcatExample02.java
@@ -0,0 +1,22 @@
+package com.example.reactor2.exam03.class04;
+
+import com.example.reactor2.utils.Logger;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+/**
+ * concat 기본 개념 예제
+ *  - 파라미터로 입력된 Iterable의 Publisher Sequence 들을 연결해서 차례대로 emit한다.
+ */
+public class ConcatExample02 {
+
+    public static void main(String[] args) {
+        List<Flux<Integer>> sources = List.of(Flux.just(1, 2, 3), Flux.just(4, 5, 6));
+
+        Flux
+                .concat(sources)
+                .subscribe(Logger::onNext);
+    }
+
+}
