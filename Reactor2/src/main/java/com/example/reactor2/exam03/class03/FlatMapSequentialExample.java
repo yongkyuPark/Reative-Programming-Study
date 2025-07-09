ba207fa2f933a4fc3dc7a7466794dec66133410b
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-07-09 21:07:48 +0900

    feat : 데이터 매핑 - flatMap 확장

---
+++ Reactor2/src/main/java/com/example/reactor2/exam03/class03/FlatMapSequentialExample.java
@@ -0,0 +1,27 @@
+package com.example.reactor2.exam03.class03;
+
+import com.example.reactor2.utils.Logger;
+import com.example.reactor2.utils.TimeUtils;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+/**
+ * flatMapSequential 기본 개념 예제
+ *  - 비동기적으로 동작할 경우에도 emit 되는 순서를 보장한다.
+ */
+public class FlatMapSequentialExample {
+
+    public static void main(String[] args) {
+        Flux
+                .range(2, 8)
+                .flatMapSequential(dan -> Flux
+                        .range(1, 9)
+                        .publishOn(Schedulers.parallel())
+                        .map(n -> dan + " * " + n + " = " + dan * n))
+
+                .subscribe(Logger::onNext);
+
+        TimeUtils.sleep(200L);
+    }
+
+}
