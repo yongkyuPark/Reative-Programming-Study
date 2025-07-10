82df3930af93f32ef3e9ed3754f9279b5749f034
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-07-10 20:17:00 +0900

    feat : 데이터 변환 기타 - then

---
+++ Reactor2/src/main/java/com/example/reactor2/exam03/class08/thenExample01.java
@@ -0,0 +1,30 @@
+package com.example.reactor2.exam03.class08;
+
+import com.example.reactor2.utils.Logger;
+import com.example.reactor2.utils.TimeUtils;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+/**
+ * then 기본 개념 예제
+ *  - Upstream Mono의 sequence가 종료되면, Mono<Void>를 Downstream으로 전달한다.
+ */
+public class thenExample01 {
+
+    public static void main(String[] args) {
+        Mono
+                .just("Hi")
+                .delayElement(Duration.ofSeconds(1))
+                .doOnNext(Logger::doOnNext)
+                .then()
+                .subscribe(
+                        Logger::onNext,
+                        Logger::onError,
+                        Logger::onComplete
+                );
+
+        TimeUtils.sleep(1500);
+    }
+
+}
---
+++ Reactor2/src/main/java/com/example/reactor2/exam03/class08/thenExample02.java
@@ -0,0 +1,36 @@
+package com.example.reactor2.exam03.class08;
+
+import com.example.reactor2.utils.Logger;
+import com.example.reactor2.utils.TimeUtils;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+/**
+ * then 활용 예제
+ *  - 1 개의 task가 모두 끝났을 때, Complete Signal을 전달해서 추가 task를 수행하는 예제
+ */
+public class thenExample02 {
+
+    public static void main(String[] args) {
+        restartApplicationServer()
+                .then()
+                .subscribe(
+                        Logger::onNext,
+                        Logger::onError,
+                        () -> Logger.onComplete("Send an email to Administrator: " +
+                                "Application Server is restarted successfully")
+                );
+
+        TimeUtils.sleep(3000L);
+    }
+
+    private static Mono<Void> restartApplicationServer() {
+        return Mono
+                .just("Application Server was restarted successfully.")
+                .delayElement(Duration.ofSeconds(2))
+                .doOnNext(Logger::doOnNext)
+                .flatMap(notUse -> Mono.empty());
+    }
+
+}
