fb91b0cd28afa5d8cdc5ef43781f089da0896a0c
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-07-10 19:41:12 +0900

    feat : 데이터 결합 - merge

---
+++ Reactor2/src/main/java/com/example/reactor2/exam03/class05/MergeExample01.java
@@ -0,0 +1,26 @@
+package com.example.reactor2.exam03.class05;
+
+import com.example.reactor2.utils.Logger;
+import com.example.reactor2.utils.TimeUtils;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+/**
+ * merge 기본 개념 예제
+ *  - 파라미터로 입력된 Publisher Sequence에서 emit된 데이터를 emit된 시간이 빠른 순서대로 merge한다.
+ */
+public class MergeExample01 {
+
+    public static void main(String[] args) {
+        Flux
+                .merge(
+                        Flux.just(1, 2, 3).delayElements(Duration.ofMillis(300L)),
+                        Flux.just(4, 5, 6).delayElements(Duration.ofMillis(500L))
+                )
+                .subscribe(Logger::onNext);
+
+        TimeUtils.sleep(3500L);
+    }
+
+}
