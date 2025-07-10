05bfeba4c566def2a92a79ab4ee9801a6340354f
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-07-10 19:54:07 +0900

    feat : 데이터 결합 - zip

---
+++ Reactor2/src/main/java/com/example/reactor2/exam03/class06/ZipExample01.java
@@ -0,0 +1,26 @@
+package com.example.reactor2.exam03.class06;
+
+import com.example.reactor2.utils.Logger;
+import com.example.reactor2.utils.TimeUtils;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+/**
+ * zip 기본 개념 예제
+ *  - 파라미터로 입력된 Publisher Sequence에서 emit된 데이터를 결합한다.
+ */
+public class ZipExample01 {
+
+    public static void main(String[] args) {
+        Flux
+                .zip(
+                        Flux.just(1, 2, 3).delayElements(Duration.ofMillis(300L)),
+                        Flux.just(4, 5, 6).delayElements(Duration.ofMillis(500L))
+                )
+                .subscribe(tuple2 -> Logger.onNext(tuple2));
+
+        TimeUtils.sleep(2500L);
+    }
+
+}
