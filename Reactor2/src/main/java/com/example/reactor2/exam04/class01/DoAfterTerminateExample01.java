65ded6c20beee1fbb7aa85c166aee7b5313802a3
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-07-12 19:31:26 +0900

    feat : peeking into sequence

---
+++ Reactor2/src/main/java/com/example/reactor2/exam04/class01/DoAfterTerminateExample01.java
@@ -0,0 +1,29 @@
+package com.example.reactor2.exam04.class01;
+
+import com.example.reactor2.utils.Logger;
+import reactor.core.publisher.Flux;
+
+/**
+ * doOnTerminate()와 doAfterTerminate()의 차이점을 이해하기 위한 예제
+ *  - doOnTerminate()는 upstream operator가 종료할 때 호출된다.
+ *  - doAfterTerminate는 전체 Sequence가 종료할 때 Downstream에서 Upstream으로 전파(propagation)되면서 호출된다.
+ */
+public class DoAfterTerminateExample01 {
+
+    public static void main(String[] args) {
+        Flux
+                .just("HI", "HELLO")
+                .filter(data -> data.equals("HI"))
+                .doOnTerminate(() -> Logger.doOnTerminate("filter"))
+                .doAfterTerminate(() -> Logger.doAfterTerminate("filter"))
+                .map(data -> data.toLowerCase())
+                .doOnTerminate(() -> Logger.doOnTerminate("map"))
+                .doAfterTerminate(() -> Logger.doAfterTerminate("map"))
+                .subscribe(
+                        data -> Logger.onNext(data),
+                        error -> {},
+                        () -> Logger.doOnComplete());
+    }
+
+
+}
---
+++ Reactor2/src/main/java/com/example/reactor2/exam04/class01/DoOnXxxxExample01.java
@@ -0,0 +1,44 @@
+package com.example.reactor2.exam04.class01;
+
+import com.example.reactor2.utils.Logger;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+
+/**
+ * DoOnXXXX() Operator의 기본 개념 예제
+ */
+public class DoOnXxxxExample01 {
+
+    public static void main(String[] args) {
+        Flux
+                .range(1, 5)
+                .doFinally(signalType -> Logger.info("doFinally() > range"))
+                .doOnNext(data -> Logger.doOnNext("range", data))
+                .doOnRequest(n -> Logger.info("doOnRequest > range: {}", 1))
+                .doOnSubscribe(subscription -> Logger.info("doOnSubscribe() > range"))
+                .doFirst(() -> Logger.info("doFirst() > range"))
+                .doOnComplete(() -> Logger.info("doOnComplete() > range"))
+                .filter(num -> num % 2 == 1)
+                .doOnNext(data -> Logger.doOnNext("filter", data))
+                .doOnRequest(n -> Logger.info("doOnRequest > filter: {}", 1))
+                .doOnSubscribe(subscription -> Logger.info("doOnSubscribe() > filter"))
+                .doFinally(signalType -> Logger.info("doFinally() > filter"))
+                .doOnComplete(() -> Logger.info("doOnComplete() > filter"))
+                .doFirst(() -> Logger.info("doFirst() > filter"))
+                .subscribe(new BaseSubscriber<>() {
+                    @Override
+                    protected void hookOnSubscribe(Subscription subscription) {
+                        request(1);
+                    }
+
+                    @Override
+                    protected void hookOnNext(Integer value) {
+                        Logger.info("# hookOnNext: {}", value);
+                        request(1);
+                    }
+                });
+    }
+
+
+}
