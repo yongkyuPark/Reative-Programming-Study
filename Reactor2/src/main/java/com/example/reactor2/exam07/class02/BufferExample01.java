5d71b251aee7ba33d14e5eac637634b0da7c614e
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-07-13 22:10:23 +0900

    feat : buffer, bufferTimeout

---
+++ Reactor2/src/main/java/com/example/reactor2/exam07/class02/BufferExample01.java
@@ -0,0 +1,20 @@
+package com.example.reactor2.exam07.class02;
+
+import com.example.reactor2.utils.Logger;
+import reactor.core.publisher.Flux;
+
+/**
+ * buffer 기본 개념 예제
+ * - Upstream에서 emit되는 데이터가 maxSize 숫자만큼 버퍼에 채워지면 버퍼를 비운다.
+ * - 버퍼에서 비워진 데이터는 List 형태로 한 번에 Downstream에 emit된다.
+ */
+public class BufferExample01 {
+
+    public static void main(String[] args) {
+        Flux
+                .range(1, 95)
+                .buffer(10)
+                .subscribe(buffer -> Logger.onNext(buffer));
+    }
+
+}
---
+++ Reactor2/src/main/java/com/example/reactor2/exam07/class02/BufferTimeoutExample01.java
@@ -0,0 +1,33 @@
+package com.example.reactor2.exam07.class02;
+
+import com.example.reactor2.utils.Logger;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+/**
+ * bufferTimeout 기본 개념 예제
+ *  - Upstream에서 emit된 데이터가 버퍼에 채워질 때, maxTime에 도달하면 버퍼를 비운다.
+ *  - maxTime에 도달하기 전에 maxSize 만큼의 데이터가 버퍼에 채워지면 maxTime까지 기다리지 않고, 버퍼를 비운다.
+ *
+ */
+public class BufferTimeoutExample01 {
+
+    public static void main(String[] args) {
+        Flux
+                .range(1, 20)
+                .map(num -> {
+                    try {
+                        if (num < 10) {
+                            Thread.sleep(100L);
+                        } else {
+                            Thread.sleep(300L);
+                        }
+                    } catch (InterruptedException e) {}
+                    return num;
+                })
+                .bufferTimeout(3, Duration.ofMillis(400L))
+                .subscribe(buffer -> Logger.onNext(buffer));
+    }
+
+}
