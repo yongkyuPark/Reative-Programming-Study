99734ed85b767db38cd069c1914fac1c23785620
Yongkyu Park <dydrbdjdsla@gmail.com>
2026-06-18 21:42:01 +0900

    feat : 리액티브 프로그래밍이란?

---
+++ webflux/src/main/java/org/example/webflux/controller/ReactiveProgrammingExampleController.java
@@ -0,0 +1,44 @@
+package org.example.webflux.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/reactive")
+public class ReactiveProgrammingExampleController {
+
+    //1~9까지 출력하는 api
+    @GetMapping("/onenine/list")
+    public List<Integer> produceOneToNine() {
+        List<Integer> sink = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+            }
+            sink.add(i);
+        }
+        return sink;
+    }
+
+    @GetMapping("/onenine/flux")
+    public Flux<Integer> produceOneToNineFlux() {
+        return Flux.create(sink -> {
+            for (int i = 1; i <= 9; i++) {
+                try {
+                    Thread.sleep(500);
+                } catch (Exception e) {
+                }
+                sink.next(i);
+            }
+            sink.complete();
+        });
+    }
+    // 리액티브 스트림 구현체 Flux, Mono를 사용하여 발생하는 데이터를 바로바로 리액티브하게 처리
+    // 비동기로 동작 - 논 블로킹하게 동작 해야한다.
+}
