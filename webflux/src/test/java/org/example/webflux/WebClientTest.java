8e2611b5f7159462b5d702c1f6cb4b35c1d919bd
Yongkyu Park <dydrbdjdsla@gmail.com>
2026-06-21 17:28:51 +0900

    feat : WebFlux에서 블로킹 처리를 하는 법

---
+++ webflux/src/test/java/org/example/webflux/WebClientTest.java
@@ -0,0 +1,32 @@
+package org.example.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+@SpringBootTest
+public class WebClientTest {
+
+    private WebClient webClient = WebClient.builder().build();
+
+    @Test
+    public void testWebClient() {
+        Flux<Integer> intFlux = webClient.get()
+                .uri("http://localhost:8080/reactive/onenine/flux")
+                .retrieve()
+                .bodyToFlux(Integer.class);
+
+        intFlux.subscribe(data -> {
+            System.out.println("처리 되고 있는 스레드 이름: " + Thread.currentThread().getName());
+            System.out.println("WebFlux가 구동 중!! : " + data);
+        });
+        System.out.println("Netty 이벤트 루프로 스레드 복귀 !!");
+        
+        try {
+            Thread.sleep(5000);
+        } catch (Exception e) {
+        }
+    }
+}
