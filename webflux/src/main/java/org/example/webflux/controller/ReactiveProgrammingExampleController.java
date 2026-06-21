c8709e15422a66d03ab05cd272557b8b73a1d838
Yongkyu Park <dydrbdjdsla@gmail.com>
2026-06-21 17:05:58 +0900

    feat : WebFlux의 Subscriber - Publisher 패턴

--- webflux/src/main/java/org/example/webflux/controller/ReactiveProgrammingExampleController.java
+++ webflux/src/main/java/org/example/webflux/controller/ReactiveProgrammingExampleController.java
@@ -1,15 +1,18 @@
 package org.example.webflux.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/reactive")
+@Slf4j
 public class ReactiveProgrammingExampleController {
 
     //1~9까지 출력하는 api
@@ -28,16 +31,17 @@ public class ReactiveProgrammingExampleController {
 
     @GetMapping("/onenine/flux")
     public Flux<Integer> produceOneToNineFlux() {
-        return Flux.create(sink -> {
+        return Flux.<Integer>create(sink -> {
             for (int i = 1; i <= 9; i++) {
                 try {
+                    log.info("현재 처리하고 있는 스레드 이름: " + Thread.currentThread().getName());
                     Thread.sleep(500);
                 } catch (Exception e) {
                 }
                 sink.next(i);
             }
             sink.complete();
-        });
+        }).subscribeOn(Schedulers.boundedElastic());
     }
     // 리액티브 스트림 구현체 Flux, Mono를 사용하여 발생하는 데이터를 바로바로 리액티브하게 처리
     // 비동기로 동작 - 논 블로킹하게 동작 해야한다.
